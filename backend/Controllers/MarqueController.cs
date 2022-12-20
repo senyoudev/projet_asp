1362b042d6f33d1aa63fff59a4cc6532e8d9d052
senyoudev <younesmeskafe2018@gmail.com>
2022-12-20 21:09:49 +0100

    get marques action in marques controller

---
+++ backend/Controllers/MarqueController.cs
@@ -0,0 +1,27 @@
+﻿using backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MarqueController : ControllerBase
+    {
+        private readonly ApiContext _db;
+
+        public MarqueController(ApiContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public JsonResult GetMarques()
+        {
+            var marques = _db.Marques.ToList();
+            if (marques == null)
+            {
+                return new JsonResult(NotFound());
+            }
+            return new JsonResult(Ok(marques));
+        }
+    }
+}
