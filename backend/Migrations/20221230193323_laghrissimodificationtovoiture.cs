3194109f6eeab26bbe7183baabf2d50e2153d6ba
senyoudev <younesmeskafe2018@gmail.com>
2022-12-30 20:34:36 +0100

    laghrissi's work

---
+++ backend/Migrations/20221230193323_laghrissimodificationtovoiture.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    public partial class laghrissimodificationtovoiture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isAprouved",
+                table: "Voitures",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isAprouved",
+                table: "Voitures");
+        }
+    }
+}
