baf4f794c39e226b81c85d77f3973c0dc613e8ee
Yassine-Chraa <yassinechraa1@gmail.com>
2022-12-30 21:08:32 +0100

    ok

--- backend/Models/inputs/UserRegister.cs
+++ backend/Models/inputs/UserRegister.cs
@@ -13,19 +13,6 @@ namespace backend.Models
         public string Role { get; set; }
         public UserType UserType { get; set; }
 
-        public string HashPassword(byte[] salt)
-        {
-           
-
-            // Hash the password using PBKDF2 with 1000 iterations
-            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: Password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
-                iterationCount: 1000,
-                numBytesRequested: 256 / 8));
-
-            return hashed;
-        }
+     
     }
 }
---
+++ backend/utils/password.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace backend.utils
+{
+    public class password
+    {
+        public static byte[] GenerateSalt()
+        {
+            // Generate a new random salt value
+            var salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+
+        public static string HashPassword(byte[] salt,string Password)
+        {
+
+
+            // Hash the password using PBKDF2 with 1000 iterations
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: Password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 1000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+    }
+}
