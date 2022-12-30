d945672bf616146ab91321f12073dc714ae4481c
senyoudev <younesmeskafe2018@gmail.com>
2022-12-30 19:59:42 +0100

    refactoring && user change password

---
+++ backend/utils/password.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace backend.utils
+{
+    public class password
+    {
+        public static byte[] GenerateSalt()
+        {
+            // Generate a new random salt value
+            var salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+    }
+}
