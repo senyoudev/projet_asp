5172e615e559bde00e61dc2ba20da864777e2c98
senyoudev <younesmeskafe2018@gmail.com>
2022-12-31 20:13:58 +0100

    not found page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Admin from "./pages/Admin";
 import AdminOwenr from "./pages/AdminOwner";
 import { AuthContextProvider } from "./Context/AuthContext";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Router>
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route path="/admin/*" element={<Admin/>}/>
         <Route path="/owner/*" element={<AdminOwenr/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
       </AuthContextProvider>
     </Router>
