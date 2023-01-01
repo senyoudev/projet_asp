2c37cbb41c5a97c679e76e1311056445cdd96698
Yassine-Chraa <yassinechraa1@gmail.com>
2023-01-01 19:46:14 +0100

    ok

--- frontend/src/Context/CarContext.js
+++ frontend/src/Context/CarContext.js
@@ -1,55 +1,58 @@
 import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
-import { getUrl } from '../API'
-import { toast } from 'react-toastify';
+import { getUrl } from "../API";
+import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-
 
 const carContext = createContext();
 
-
-
 export const useCar = () => {
-    const context = useContext(carContext);
-    if (!context) throw new Error("Car Provider is missing");
-    return context;
+  const context = useContext(carContext);
+  if (!context) throw new Error("Car Provider is missing");
+  return context;
 };
 
-const carUrl = getUrl('Cars')
+const carUrl = getUrl("Cars");
 
-const userInfo = localStorage.getItem('userInfo')
-  ? JSON.parse(localStorage.getItem('userInfo'))
-  : null
+const userInfo = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 export const CarContextProvider = ({ children }) => {
-    
-    const navigate = useNavigate()
-    const [loading,setLoading] = useState(false)
-    
-      const getOwnerCarsNumber = async(id) => {
-        setLoading(true)
-            try {
-                const { data } = await axios.get(`${carUrl}/countByUser`,{id});
-                setLoading(false)
-                console.log(data)
-                return data  
-            } catch (error) {
-                toast.error("Something went wrong")
-                console.log(error.response)
-                setLoading(false)
-            }
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const getOwnerCars = async () => {
+    try {
+      const { data } = await axios.get(`${carUrl}/api/Voiture`);
+      setLoading(false);
+      return data
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.log(error.response);
+      setLoading(false);
     }
-    
-   
+  };
+  const getOwnerCarsNumber = async (id) => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(`${carUrl}/countByUser`, { id });
+      setLoading(false);
+      return data;
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.log(error.response);
+      setLoading(false);
+    }
+  };
 
-
-    return (
-        <carContext.Provider
-            value={{
-              getOwnerCarsNumber
-            }}
-        >
-            {children}
-        </carContext.Provider>
-    );
+  return (
+    <carContext.Provider
+      value={{
+        getOwnerCarsNumber,
+        getOwnerCars,
+      }}
+    >
+      {children}
+    </carContext.Provider>
+  );
 };
--- frontend/src/pages/owner/Cars.jsx
+++ frontend/src/pages/owner/Cars.jsx
@@ -1,13 +1,33 @@
 import { Col, Form, Row } from "react-bootstrap";
 import CarsTable from "../../components/tables/CarsTable";
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useCar } from "../../Context/CarContext";
+import { useState } from "react";
 function Cars() {
+  const { getOwnerCars } = useCar();
+  const [userInfo, setUserInfo] = useState(
+    JSON.parse(localStorage.getItem("userInfo"))
+  );
+  const [carsList,setCarsList] = useState([]);
+  const fetchData = async () => {
+    const data = await getOwnerCars();
+    setCarsList(data.value);
+  };
+  useEffect(() => {
+    if (userInfo != null && userInfo.role == "proprietaire") {
+      fetchData();
+    }
+  }, [localStorage.getItem("userInfo")]);
+  console.log(carsList)
   return (
     <>
       <Row className="mb-4">
         <Col md="6">
           <Form.Select aria-label="Car Status">
-            <option value="1" defaultChecked>All</option>
+            <option value="1" defaultChecked>
+              All
+            </option>
             <option value="2">Available Cars</option>
             <option value="3">Reserved Cars</option>
           </Form.Select>
@@ -20,7 +40,7 @@ function Cars() {
           />
         </Col>
       </Row>
-      <CarsTable type="owner" />
+      {carsList !== [] ? <CarsTable type="owner" data={carsList} />: null}
     </>
   );
 }
