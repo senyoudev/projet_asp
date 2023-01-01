b3a48d425486096ee3855ea8d52400172aa9cc38
senyoudev <younesmeskafe2018@gmail.com>
2023-01-01 17:11:49 +0100

    fix conflict

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import {
   faClock,
   faEarth,
   faPhone,
+  faSign,
   faSignIn,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
@@ -14,38 +15,38 @@ import "../../assets/css/header.css";
 import navLinks from "../../assets/Data/navLinks";
 import { useAuth } from "../../Context/AuthContext";
 import Navbar from "../Navbars/Navbar";
-import { LinkContainer } from "react-router-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
+
+
 
 const Header = () => {
-  const { logout } = useAuth("");
-  const [userInfo, setUserInfo] = useState(null);
-  const logoutHandler = () => {
-    logout();
-  };
+
+    const {logout} = useAuth('')
+    const [userInfo,setUserInfo] = useState(null)
+    const logoutHandler = () => {
+     logout()
+  }
 
   useEffect(() => {
-<<<<<<< HEAD
-    if( localStorage.getItem('userInfo')) {
-      setUserInfo(JSON.parse(localStorage.getItem('userInfo')))
-    }
+    const userInfo = localStorage.getItem('userInfo')
+  ? setUserInfo(JSON.parse(localStorage.getItem('userInfo'))) : null
   
   },[localStorage.getItem('userInfo')])
 
-=======
-    const userInfo = localStorage.getItem("userInfo")
-      ? setUserInfo(JSON.parse(localStorage.getItem("userInfo")))
-      : null;
-  }, [localStorage.getItem("userInfo")]);
->>>>>>> origin/main
 
-  const UserMenu = (
+
+ 
+   const UserMenu = (
     <Image
       src={userInfo?.photo || process.env.REACT_APP_USER_IMAGE}
       alt="UserName profile image"
       roundedCircle={true}
-      style={{ width: "30px", height: "30px", border: "1px solid #3b8ac3" }}
+      style={{ width: '30px', height: '30px', border: '1px solid #3b8ac3' }}
     />
-  );
+  )
+
+
+
 
   return (
     <header className="header">
@@ -64,7 +65,6 @@ const Header = () => {
 
             <Col lg="6" md="6" sm="6">
               <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
-<<<<<<< HEAD
                 {
                   userInfo ? (
                   
@@ -82,8 +82,8 @@ const Header = () => {
                             )}
 
                              {userInfo.role == 'Administrator' && (
-                                <LinkContainer to='/admin'>
-                                  <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                                <LinkContainer to='/owner'>
+                                  <NavDropdown.Item>Profile</NavDropdown.Item>
                                 </LinkContainer>
                             )}
                                
@@ -111,49 +111,16 @@ const Header = () => {
                     </>
                         )
                 }
-=======
-                {userInfo ? (
-                  <NavDropdown title={UserMenu} id="username">
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to="/orders">
-                      <NavDropdown.Item>Orders</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to="/settings">
-                      <NavDropdown.Item>Settings</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                ) : (
-                  <>
-                    <Link
-                      to="/login"
-                      className=" d-flex align-items-center gap-1"
-                    >
-                      <FontAwesomeIcon icon={faSignIn} /> Login
-                    </Link>
-
-                    <Link
-                      to="/register"
-                      className=" d-flex align-items-center gap-1"
-                    >
-                      <FontAwesomeIcon icon={faUser} /> Register
-                    </Link>
-                  </>
-                )}
->>>>>>> origin/main
               </div>
             </Col>
           </Row>
         </Container>
       </div>
 
+      
       {/* ========== main navigation =========== */}
 
-      <Navbar navLinks={navLinks} />
+     <Navbar navLinks={navLinks}/>
     </header>
   );
 };
