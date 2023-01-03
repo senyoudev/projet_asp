2fefdd655aa2889ff94f8eea53ba6d341a918f65
senyoudev <younesmeskafe2018@gmail.com>
2023-01-03 08:47:28 +0100

    update 2

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -49,77 +49,73 @@ const Header = () => {
 
 
   return (
-    <header className="header">
+    <header className='header'>
       {/* ============ header top ============ */}
-      <div className="header__top">
+      <div className='header__top'>
         <Container>
           <Row>
-            <Col lg="6" md="6" sm="6">
-              <div className="header__top__left">
+            <Col lg='6' md='6' sm='6'>
+              <div className='header__top__left'>
                 <span>Need Help?</span>
-                <span className="header__top__help">
-                  <FontAwesomeIcon icon={faPhone} /> +1-202-555-0149
+                <span className='header__top__help'>
+                  <FontAwesomeIcon icon={faPhone} /> +212554579699
                 </span>
               </div>
             </Col>
 
-            <Col lg="6" md="6" sm="6">
-              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
-                {
-                  userInfo ? (
-                  
-                    <NavDropdown title={UserMenu} id='username'>
-                                <LinkContainer to='/profile'>
-                                  <NavDropdown.Item>Profile</NavDropdown.Item>
-                                </LinkContainer>
-                            
+            <Col lg='6' md='6' sm='6'>
+              <div className='header__top__right d-flex align-items-center justify-content-end gap-3'>
+                {userInfo ? (
+                  <NavDropdown title={UserMenu} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
 
-                             {userInfo.role == 'proprietaire' && (
-                                <LinkContainer to='/owner'>
-                                  <NavDropdown.Item>Dashboard</NavDropdown.Item>
-                                </LinkContainer>
-                            )}
+                    {userInfo.role == 'proprietaire' && (
+                      <LinkContainer to='/owner'>
+                        <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                      </LinkContainer>
+                    )}
 
-                             {userInfo.role == 'Administrator' && (
-                                <LinkContainer to='/admin'>
-                                  <NavDropdown.Item>Dashboard</NavDropdown.Item>
-                                </LinkContainer>
-                            )}
-                               
-                             
-                                <LinkContainer to='/settings'>
-                                  <NavDropdown.Item>Settings</NavDropdown.Item>
-                                </LinkContainer>
-                                <NavDropdown.Item onClick={logoutHandler}>
-                                  Logout
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        ): (
-                              <>
-                          <Link to="/login" className=" d-flex align-items-center gap-1">
-                        <i class="ri-login-circle-line"></i> Login
-                      </Link>
+                    {userInfo.role == 'Administrator' && (
+                      <LinkContainer to='/admin'>
+                        <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                      </LinkContainer>
+                    )}
 
+                    <LinkContainer to='/settings'>
+                      <NavDropdown.Item>Settings</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <>
+                    <Link
+                      to='/login'
+                      className=' d-flex align-items-center gap-1'
+                    >
+                      <i class='ri-login-circle-line'></i> Login
+                    </Link>
 
-                      <Link
-                        to="/register"
-                        className=" d-flex align-items-center gap-1"
-                      >
-                        <FontAwesomeIcon icon={faUser} /> Register
-                      </Link>
-                    </>
-                        )
-                }
+                    <Link
+                      to='/register'
+                      className=' d-flex align-items-center gap-1'
+                    >
+                      <FontAwesomeIcon icon={faUser} /> Register
+                    </Link>
+                  </>
+                )}
               </div>
             </Col>
           </Row>
         </Container>
       </div>
 
-      
       {/* ========== main navigation =========== */}
 
-     <Navbar navLinks={navLinks}/>
+      <Navbar navLinks={navLinks} />
     </header>
   );
 };
