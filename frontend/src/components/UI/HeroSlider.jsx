c3723110b99625c10560b5299942d100c3984695
senyoudev <younesmeskafe2018@gmail.com>
2023-01-03 08:45:00 +0100

    update the content of the homepage

--- frontend/src/components/UI/HeroSlider.jsx
+++ frontend/src/components/UI/HeroSlider.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import Slider from "react-slick";
-import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import Slider from 'react-slick';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-import "../../assets/css/hero-slider.css";
+import '../../assets/css/hero-slider.css';
 
 const HeroSlider = () => {
   const settings = {
@@ -18,41 +18,47 @@ const HeroSlider = () => {
     pauseOnHover: false,
   };
   return (
-    <Slider {...settings} className="hero__slider">
-      <div className="slider__item slider__item-01 mt0">
+    <Slider {...settings} className='hero__slider'>
+      <div className='slider__item slider__item-01 mt0'>
         <Container>
-          <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+          <div className='slider__content '>
+            <h4 className='text-light mb-3'>
+              For Rent Starting from 100Dhs Per Day
+            </h4>
+            <h1 className='text-light mb-4'>Reserve Now and Get Your Car</h1>
 
-            <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+            <button className='btn reserve__btn mt-4'>
+              <Link to='/cars'>Reserve Now</Link>
             </button>
           </div>
         </Container>
       </div>
 
-      <div className="slider__item slider__item-02 mt0">
+      <div className='slider__item slider__item-02 mt0'>
         <Container>
-          <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+          <div className='slider__content '>
+            <h4 className='text-light mb-3'>
+              For Rent Starting from 100Dhs Per Day
+            </h4>
+            <h1 className='text-light mb-4'>Reserve Now and Get Your Car</h1>
 
-            <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+            <button className='btn reserve__btn mt-4'>
+              <Link to='/cars'>Reserve Now</Link>
             </button>
           </div>
         </Container>
       </div>
 
-      <div className="slider__item slider__item-03 mt0">
+      <div className='slider__item slider__item-03 mt0'>
         <Container>
-          <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+          <div className='slider__content '>
+            <h4 className='text-light mb-3'>
+              For Rent Starting from 100Dhs Per Day
+            </h4>
+            <h1 className='text-light mb-4'>Reserve Now and Get Your Car</h1>
 
-            <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+            <button className='btn reserve__btn mt-4'>
+              <Link to='/cars'>Reserve Now</Link>
             </button>
           </div>
         </Container>
